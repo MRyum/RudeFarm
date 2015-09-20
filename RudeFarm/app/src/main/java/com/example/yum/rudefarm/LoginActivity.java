427f842a781a5bde6721eaa80e21a4042af03370
MRyum <yum980801@gmail.com>
2015-09-20 11:04:31 +0900

    changed

--- RudeFarm/app/src/main/java/com/example/yum/rudefarm/LoginActivity.java
+++ RudeFarm/app/src/main/java/com/example/yum/rudefarm/LoginActivity.java
@@ -82,6 +82,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onPause() {
+        super.onPause();
         background = (ImageView) findViewById(R.id.background);
         ((BitmapDrawable) background.getDrawable()).getBitmap().recycle();
         ImageView[] dum = {cloud1, cloud2, cloud3};
