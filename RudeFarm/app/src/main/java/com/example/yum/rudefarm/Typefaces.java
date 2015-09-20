d488489875f601ac373640771028e8638e05a6a4
MRyum <yum980801@gmail.com>
2015-09-20 10:08:26 +0900

    RudeFarm

---
+++ RudeFarm/app/src/main/java/com/example/yum/rudefarm/Typefaces.java
@@ -0,0 +1,21 @@
+package com.example.yum.rudefarm;
+
+import android.graphics.Typeface;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
+/**
+ * Created by YUM on 2015-09-11.
+ */
+public class Typefaces {
+
+    static Typeface typeface;
+
+    static public void setFont(String fontName, TextView tv, View view) {
+        typeface = Typeface.createFromAsset(view.getContext().getAssets(), fontName);
+        tv.setTypeface(typeface);
+    }
+
+}
